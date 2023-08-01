9bf47c580c219dc3d67fcbeda1d00221883d11cb
Geyser Ramírez <geyser.grs@gmail.com>
2023-08-01 16:21:14 -0600

    Update App.test.tsx
    Add some test for game components, rendering and user interaction

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,29 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import App from '../App';
 
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+describe('App', () => {
+  it('should render the game board', () => {
+    const {getByTestId} = render(<App />);
+    const board = getByTestId('board');
+    expect(board).toBeTruthy();
+    expect(board.children.length).toBe(4);
+  });
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+  it('should render the status text', () => {
+    const {getByTestId} = render(<App />);
+    const status = getByTestId('status');
+    expect(status).toBeTruthy();
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+    expect(status.props.children).toBe('Next player: X');
+  });
+
+  it('should update the game state when a cell is tapped', () => {
+    const {getAllByTestId, getByTestId} = render(<App />);
+    const squares = getAllByTestId('square');
+    fireEvent.press(squares[0]);
+    waitFor(() => {
+      expect(getByTestId('status').props.children).toBe('Next player: O');
+    });
+  });
 });
