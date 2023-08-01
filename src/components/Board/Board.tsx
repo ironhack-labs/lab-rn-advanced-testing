97580e815ca25744f5beeb786755b3698c64458b
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-08-01 15:19:40 -0600

    chore(*): Iteration 1: fail test

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+export const board: string[][] = [
+  ["","",""],
+  ["","",""],
+  ["","",""],
+]
+
+export const Board = () => {
+
+  return (
+    <View>
+      <Text>Board</Text>
+    </View>
+  )
+}
