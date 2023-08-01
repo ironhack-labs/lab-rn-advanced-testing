1b2abf3eb620430e141d23b1c757a6e4c324c07d
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-08-01 17:01:08 -0600

    chore: add jest setup
    Co-authored-by: michellepennat-df <michellepennat-df@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@ui-kitten)',
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.ts',
+    '@testing-library/jest-native/extend-expect',
+  ],
 };
